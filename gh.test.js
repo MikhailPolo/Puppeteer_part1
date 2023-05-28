b1559c0f51a673477abc15863644e0554a4a5710
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2023-05-28 14:20:36 +0500

    initial commit

---
+++ gh.test.js
@@ -0,0 +1,72 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  }, 100000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 15000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  }, 20000);
+});
+
+describe("Github page sponsors tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/sponsor");
+  });
+
+  test("The h1 header sponsor content", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual("sponsor · GitHub");
+  }, 25000);
+
+  test("The first link attribute", async () => {
+  const actual = await page.$eval("a", link => link.getAttribute('href'));
+  expect(actual).toEqual("#start-of-content")
+  }, 30000);
+});
+
+describe("Github page marketplace", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/marketplace");
+  });
+  test("The h1 header marketplace content", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub Marketplace · to improve your workflow · GitHub");
+  }, 35000);
+});
